04267ebe4f98158603fa544aa5228d588ee076b1
Jesus_Alvarado-Huayhuaz <inefable12@gmail.com>
2024-12-22 17:30:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.title("Visualizador de Moléculas 3D")
 st.markdown("Autor: Jesus Alvarado-Huayhuaz")
 
 # Ruta del archivo mol2
-github_url = "https://raw.githubusercontent.com/inefable12/fe_siderophores_database/main/complexes/Fe_sideroforos_mol21.mol2"
+url = "https://raw.githubusercontent.com/inefable12/fe_siderophores_database/main/complexes/Fe_sideroforos_mol21.mol2"
 mol = MolFromMol2File(url, sanitize=True)
 mol_block = Chem.MolToMolBlock(mol)            
 showmol(mol_block, width=800, height=400)
