bff4173a6637979112086ac4b74fded06d4535a8
Jesus_Alvarado-Huayhuaz <inefable12@gmail.com>
2024-12-22 17:41:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,18 @@ from rdkit.Chem import SanitizeMol
 st.title("Visualizador de Moléculas 3D")
 st.markdown("Autor: Jesus Alvarado-Huayhuaz")
 
-# Ruta del archivo mol2
-url = "https://raw.githubusercontent.com/inefable12/fe_siderophores_database/main/complexes/Fe_sideroforos_mol21.mol2"
-mol = MolFromMol2File(url) #, sanitize=True)
-#mol_block = Chem.MolToMolBlock(mol)            
-showmol(mol) #, width=800, height=400)
+from rdkit import Chem
+from rdkit.Chem import rdmolfiles
+
+# Ruta al archivo MOL2
+mol2_file = "https://raw.githubusercontent.com/inefable12/fe_siderophores_database/main/complexes/Fe_sideroforos_mol21.mol2"
+
+# Leer el archivo MOL2
+mol = rdmolfiles.MolFromMol2File(mol2_file)
+
+if mol:
+    # Convertir a SMILES
+    smiles = Chem.MolToSmiles(mol)
+    print(f"SMILES: {smiles}")
+else:
+    print("No se pudo leer el archivo MOL2.")
