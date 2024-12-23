cdf69c4ce7b6f1093c0144bbf08e15878f6a1a19
Jesus_Alvarado-Huayhuaz <inefable12@gmail.com>
2024-12-23 01:31:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def fetch_mol2_content(url):
 def visualize_molecule(mol_block):
     viewer = py3Dmol.view(width=800, height=400)
     viewer.addModel(mol_block, "mol")
-    viewer.setStyle({"elem": "Fe", "sphere": {"radius": 0.3, "color": "blue"}}) #metal
+    viewer.setStyle({"elem": "FE", "sphere": {"radius": 0.3, "color": "blue"}}) #metal
     #viewer.setStyle({"stick": {}})
     viewer.setStyle({"elem": "C", "stick": {}})
     viewer.setStyle({"elem": "H", "stick": {}})
